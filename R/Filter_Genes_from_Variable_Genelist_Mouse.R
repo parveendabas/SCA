839ae884a410fb1235586fbf8071821f7aa670bd
parveenkgit <parveen.kumar@jax.org>
2020-07-20 14:47:43 -0400

    SCA on Github

---
+++ R/Filter_Genes_from_Variable_Genelist_Mouse.R
@@ -0,0 +1,38 @@
+#' A Filter_Genes_from_Variable_Genelist_Mouse Function
+#'
+#' This function allows you to express your love of cats.
+#' @param SCdata Seurat Object
+#' @param mt mitochondrial genes (MOUSE) to be removed from Seurat object 
+#' @param rb ribosomal genes (MOUSE) to be removed from Seurat object 
+#' @param cc CellCycle genes (Converted from Human to Mouse) to be removed from Seurat object 
+#' @keywords SCdata, mt, rb, cc
+#' @export
+#' @examples
+#' Filter_Genes_from_Variable_Genelist_Mouse()
+
+Filter_Genes_from_Variable_Genelist_Mouse <- function(SCdata, mt = TRUE, rb = TRUE, cc = FALSE){
+  
+  print(paste0("Fetching mitochondrial and ribosomal genes"))
+  mt.genes <- rownames(SCdata@assays$RNA@counts)[rownames(SCdata@assays$RNA@counts) %like% "^mt-"]; mt.genes; length(mt.genes)
+  rb.genes <- rownames(SCdata@assays$RNA@counts)[rownames(SCdata@assays$RNA@counts) %like% "^Rp[sl]"]; rb.genes; length(rb.genes)
+  
+  if(cc == TRUE){
+  print(paste0("Adding cell cycle genes to filter list"))  
+  s.genes <- cc.genes$s.genes; head(s.genes); length(s.genes)
+  s.genes <- stringr::str_to_title((s.genes)); head(s.genes); length(s.genes)
+  g2m.genes <- cc.genes$g2m.genes; head(g2m.genes); length(g2m.genes)
+  g2m.genes <- stringr::str_to_title((g2m.genes)); head(g2m.genes); length(g2m.genes)
+  cellcycle.genes <- c(s.genes, g2m.genes); length(cellcycle.genes)
+  genes.filter <- c(mt.genes, rb.genes, cellcycle.genes); head(genes.filter); print(length(genes.filter))
+  } else {
+    genes.filter <- c(mt.genes, rb.genes); head(genes.filter); print(length(genes.filter))
+  }
+  
+  return(genes.filter)
+  
+  print("Done")
+  print(Sys.time())
+}
+
+
+
