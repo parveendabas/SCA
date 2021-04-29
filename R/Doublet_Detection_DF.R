065d2e282eb2f2acf5fa7493a688f09f0ac1687c
parveenkgit <parveen.kumar@jax.org>
2021-04-29 17:25:08 -0400

    SCA on Github

--- R/Doublet_Detection_DF.R
+++ R/Doublet_Detection_DF.R
@@ -59,11 +59,11 @@ Doublet_Detection_DF <- function(SeuratObject, saveDIR, Sample, Species="hsa", F
     print(head(SeuratObject@meta.data))
     
     # Visualize QC metrics as a violin plot
-    p1 <- VlnPlot(SeuratObject, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), cols = ClusPallette, pt.size = 0.00, ncol = 1)
-    print(p1)
-    plot1 <- FeatureScatter(SeuratObject, feature1 = "nCount_RNA", feature2 = "percent.mt", cols = ClusPallette)
-    plot2 <- FeatureScatter(SeuratObject, feature1 = "nCount_RNA", feature2 = "nFeature_RNA", cols = ClusPallette)
-    print(CombinePlots(plots = list(plot1, plot2)))
+    #p1 <- VlnPlot(SeuratObject, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), cols = ClusPallette, pt.size = 0.00, ncol = 1)
+    #print(p1)
+    #plot1 <- FeatureScatter(SeuratObject, feature1 = "nCount_RNA", feature2 = "percent.mt", cols = ClusPallette)
+    #plot2 <- FeatureScatter(SeuratObject, feature1 = "nCount_RNA", feature2 = "nFeature_RNA", cols = ClusPallette)
+    #print(CombinePlots(plots = list(plot1, plot2)))
     
     print("Finished Plotting")
     
